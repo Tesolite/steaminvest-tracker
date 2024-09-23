5bd87f038b478f718a358089839c32d70c91fd96
Tesolite <pavel.kotka@gmail.com>
2024-09-23 17:58:16 +0100

    added form input processing + improved form pattern validation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,50 @@
-console.log("hello world!");
+var form = document.getElementById("investment-form");
+var formURL = document.getElementById("in-url");
+var formQuantity = document.getElementById("in-quantity");
+var formCurrencyCode = document.getElementById("in-currency");
+var formCost = document.getElementById("in-cost");
+var saveFormData = function () {
+    var investmentURL = formURL.value;
+    var investmentQuantity = Number(formQuantity.value);
+    var investmentCurrencyCode = Number(formCurrencyCode.value);
+    //Needs to change commas into decimal points
+    var investmentCost = Number(formCost.value);
+    var userInfo = {
+        itemURL: investmentURL,
+        quantity: investmentQuantity,
+        currencyCode: investmentCurrencyCode,
+        cost: investmentCost,
+    };
+    if (localStorage.getItem("investments") === null) {
+        var investments = [];
+        investments.push(userInfo);
+        localStorage.setItem("investments", JSON.stringify(investments));
+    }
+    else {
+        var storedInvestments = JSON.parse(localStorage.getItem("investments"));
+        storedInvestments.push(userInfo);
+        localStorage.setItem("investments", JSON.stringify(storedInvestments));
+    }
+};
+var resetForm = function () {
+    formURL.value = "";
+    formQuantity.value = "";
+    formCost.value = "";
+};
+if (form.onsubmit) {
+    form.onsubmit = function (event) {
+        event.preventDefault();
+        if (form.checkValidity()) {
+            saveFormData();
+            alert("Investment registered");
+            resetForm();
+        }
+        else {
+            form.reportValidity();
+        }
+    };
+}
+var printFormData = function () {
+    console.log("Unparsed:" + localStorage.getItem("investments"));
+    console.log("Parsed:" + JSON.parse(localStorage.getItem("investments")));
+};
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,68 @@
-console.log("hello world!");
+type UserInvestment = {
+  itemURL: string;
+  quantity: number;
+  currencyCode: number;
+  cost: number;
+};
+
+const form = document.getElementById("investment-form") as HTMLFormElement;
+const formURL = document.getElementById("in-url") as HTMLInputElement;
+const formQuantity = document.getElementById("in-quantity") as HTMLInputElement;
+const formCurrencyCode = document.getElementById(
+  "in-currency",
+) as HTMLInputElement;
+const formCost = document.getElementById("in-cost") as HTMLInputElement;
+
+const saveFormData = (): void => {
+  const investmentURL: string = formURL.value;
+
+  const investmentQuantity: number = Number(formQuantity.value);
+
+  const investmentCurrencyCode: number = Number(formCurrencyCode.value);
+
+  //Needs to change commas into decimal points
+  const investmentCost: number = Number(formCost.value);
+
+  const userInfo: UserInvestment = {
+    itemURL: investmentURL,
+    quantity: investmentQuantity,
+    currencyCode: investmentCurrencyCode,
+    cost: investmentCost,
+  };
+
+  if (localStorage.getItem("investments") === null) {
+    let investments: Array<UserInvestment> = [];
+    investments.push(userInfo);
+    localStorage.setItem("investments", JSON.stringify(investments));
+  } else {
+    let storedInvestments: Array<UserInvestment> = JSON.parse(
+      localStorage.getItem("investments")!,
+    );
+    storedInvestments.push(userInfo);
+    localStorage.setItem("investments", JSON.stringify(storedInvestments));
+  }
+};
+
+const resetForm = () => {
+  formURL.value = "";
+  formQuantity.value = "";
+  formCost.value = "";
+};
+
+if (form.onsubmit) {
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    if (form.checkValidity()) {
+      saveFormData();
+      alert("Investment registered");
+      resetForm();
+    } else {
+      form.reportValidity();
+    }
+  };
+}
+
+const printFormData = () => {
+  console.log("Unparsed:" + localStorage.getItem("investments"));
+  console.log("Parsed:" + JSON.parse(localStorage.getItem("investments")!));
+};
